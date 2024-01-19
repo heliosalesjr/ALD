71e926d504824a5ca052a8c07da512148bae1e18
heliosalesjr <heliosalesjr@gmail.com>
2024-01-19 16:08:11 -0300

    footer adjustment

--- ald-6-1/src/app/components/Atividades/AtiFerramentas.jsx
+++ ald-6-1/src/app/components/Atividades/AtiFerramentas.jsx
@@ -6,7 +6,7 @@ function AtiFerramentas() {
   return (
     <>
       <div className='mx-auto max-w-screen-xl '>
-        <h2 className='font-bold text-2xl md:text-4xl sm:text-3xl text-gray-700 py-4 text-center'>Ferramentas que podem ser utilizadas para fazer esse diagnóstico</h2>
+        <h2 className='font-bold text-4xl text-slate-700 py-4 text-center'>Ferramentas que podem ser utilizadas para fazer esse diagnóstico</h2>
         <p className="text-gray-800 text-center">Criamos um link para cada uma das sugestões de ferramentas já com o modelo de aplicação com os estudantes. Clique em cada uma dessas ferramentas para conhecê-las e então aplicá-las com os estudantes por meio da plataforma Google:</p>
       </div>
       <motion.div className="grid grid-cols-1 mx-auto max-w-screen-xl md:grid-cols-3 sm:grid-cols-1 gap-8 px-8"
